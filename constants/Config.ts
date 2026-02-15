55e47c7fc797f35a9017d81ad5378555195b6549
Amit <vamit20000@gmail.com>
2026-02-15 23:58:00 +0530

    Update Android API base URL for testing environment

--- constants/Config.ts
+++ constants/Config.ts
@@ -7,7 +7,7 @@ const getApiBaseUrl = () => {
         // Use LAN IP for physical Android device testing (from logs: 192.168.1.5)
         // If using Emulator, you can switch back to 'http://10.0.2.2:8000/api'
         if (Platform.OS === 'android') {
-            return 'http://192.168.1.5:8000/api';
+             return 'http://192.168.29.144:8000/api';
         }
         // For iOS Simulator, localhost usually works fine
         if (Platform.OS === 'ios') {
